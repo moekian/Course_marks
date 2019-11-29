2dd00bb96a91cc9d1586e0230f1626b3dc144229
Mohammad Kiani <kianimohammad@outlook.com>
2019-11-28 20:07:25 -0500

    Course Marks, layout Demo

---
+++ app/src/main/java/com/f19/layoutdemo/Activities/FrameActivity.java
@@ -0,0 +1,16 @@
+package com.f19.layoutdemo.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.f19.layoutdemo.R;
+
+public class FrameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame);
+    }
+}
